9241a41ef6705ec90a8fa520064c76f049d980b4
2shou-Clone <2shoucode@gmail.com>
2026-03-07 22:34:01 +0800

    Allow GitHub avatars in Next image config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
